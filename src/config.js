ea0b55ee8380bd189f85c0959bd2a22e43eed3f3
Aishwarya S <aishwarya@carmanetworks.com>
2018-07-04 16:49:35 +0530

    URLs changed to static IP

--- src/config.js
+++ src/config.js
@@ -1,8 +1,7 @@
+export const API_URL = 'http://183.82.32.121:8090/';
 
-export const API_URL = 'http://192.168.1.10:8090/';
-
-export const SOCKET_URL = 'http://192.168.1.5:8088';
+export const SOCKET_URL = 'http://183.82.32.121:8088';
 
 export const START_SCRIPT_COMMAND = '/home/murali/py/sim_stop.py;/home/murali/py/sim_start.py';
 
-export const SUBSCRIPTION_URL = "192.168.1.5";
+export const SUBSCRIPTION_URL = "192.168.1.26";

bd4c5fc02d8025a6fce24d62893bdb77334d9b54
Aishwarya S <aishwarya@carmanetworks.com>
2018-07-30 15:31:19 +0530

    Warnings component refactor

--- src/components/warnings.js
+++ src/components/warnings.js
@@ -2,59 +2,28 @@
 import React, {Component} from 'react';
 import '../css/warnings.css';
 import { connect } from 'react-redux';
+import { warnings } from "../constants";
 
 class Warnings extends Component {
 
   render() {
-    
-  return (<div id="warnings" className="clearfix">
-    <div className="warrning_label">FCW
-      <span className="tooltiptext">Forward Collision Warning</span>:
+   let warningData = [];
+      for(let key in warnings){
+      let value = warnings[key];
+        warningData.push(
+          <div key={key + "-container"}>
+              <div className="warrning_label">{key}
+                  <span className="tooltiptext">{value}</span>:
+              </div>
+              <div className="warrning_desc">{this.props.warningCount[key]}</div>
+          </div>
+        );
+      }
+  return (
+    <div id="warnings" className="clearfix">
+        {warningData}
     </div>
-    <div className="warrning_desc">{this.props.warningCount.FCW}</div>
-    <div className="warrning_label">FRCA
-      <span className="tooltiptext">Front Right Collision Alert</span>:
-    </div>
-    <div id="FRCA" className="warrning_desc">{this.props.warningCount.FRCA}</div>
-    <div className="warrning_label">FLCA
-      <span className="tooltiptext">Front Left Collision Alert</span>:
-    </div>
-    <div id="FLCA" className="warrning_desc">{this.props.warningCount.FLCA}
-    </div>
-    <div className="warrning_label">ICW
-      <span className="tooltiptext">Intersection Collision Warning</span>:
-    </div>
-    <div id="ICW" className="warrning_desc">{this.props.warningCount.ICW}</div>
-    <div className="warrning_label">EBW
-      <span className="tooltiptext">Emergency Brake Warning</span>:
-    </div>
-    <div id="EBW" className="warrning_desc">{this.props.warningCount.EBW}</div>
-    <div className="warrning_label">SMVA
-      <span className="tooltiptext">Slow Moving Vehicle Ahead</span>:
-    </div>
-    <div id="SMVA" className="warrning_desc">{this.props.warningCount.SMVA}</div>
-    <div className="warrning_label">SVA
-      <span className="tooltiptext">Stopped Vehicle Ahead</span>:
-    </div>
-    <div id="SVA" className="warrning_desc">{this.props.warningCount.SVA}</div>
-    <div className="warrning_label">BSW
-      <span className="tooltiptext">Blind Spot Warning
-      </span>:
-    </div>
-    <div id="BSW" className="warrning_desc">{this.props.warningCount.BSW}</div>
-    <div className="warrning_label">LCW
-      <span className="tooltiptext">Lane Change Warning</span>:
-    </div>
-    <div id="LCW" className="warrning_desc">{this.props.warningCount.LCW}</div>
-    <div className="warrning_label">RRCA
-      <span className="tooltiptext">Rear Right Collision Alert</span>:
-    </div>
-    <div id="RRCA" className="warrning_desc">{this.props.warningCount.RRCA}</div>
-    <div className="warrning_label">RLCA
-      <span className="tooltiptext">Rear Left Collision Alert</span>:
-    </div>
-    <div id="RLCA" className="warrning_desc">{this.props.warningCount.RLCA}</div>
-  </div>);
+  );
 
   }
 }
--- src/constants.js
+++ src/constants.js
@@ -17,4 +17,19 @@ export const warningsInitialState = {
 	"RLCA": 0
 };
 
+export const warnings = {
+"FCW": "Forward Collision Warning",
+"FRCA": "Front Right Collision Alert",
+"FLCA": "Front Left Collision Alert",
+"ICW": "Intersection Collision Warning",
+"EBW": "Emergency Brake Warning",
+"SMVA": "Slow Moving Vehicle Ahead",
+"SVA": "Stopped Vehicle Ahead",
+"BSW": "Blind Spot Warning",
+"LCW": "Lane Change Warning",
+"RRCA": "Rear Right Collision Alert",
+"RLCA": "Rear Left Collision Alert"
+
+};
+ 
 window.socketStart = false;
